fa5963d3d743fa11a64dd54bd0fe1f4c8abe4a81
Caroline Nordbrandt <caroline_nordbradt@hotmail.com>
2026-02-04 14:27:26 +0100

    Remove unused `assertThat` import in `PriceWatcherAsyncTest`.

--- src/test/java/org/example/weather/price/PriceWatcherAsyncTest.java
+++ src/test/java/org/example/weather/price/PriceWatcherAsyncTest.java
@@ -14,7 +14,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class PriceWatcherAsyncTest {
